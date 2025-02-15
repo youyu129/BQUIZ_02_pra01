db728c68a0df76f83ee24cf5e5cef8dc4dd9e5d0
youyu <129youyu@gmail.com>
2025-02-15 22:06:46 +0800

    新增標題圖片 替代文字 連結回首頁

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
     </div>
     <div id="all">
         <div id="title">
-            00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
+            02 月 15 號 Saturday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
         <div id="title2">
-
+            <a href="index.php"><img src="./icon/02B01.jpg" alt="健康促進網 - 回首頁"></a>
         </div>
         <div id="mm">
             <div class="hal" id="lef">
