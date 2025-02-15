022a1fced79333b21d4fc147d294e52e8abe11a4
youyu <129youyu@gmail.com>
2025-02-15 21:37:42 +0800

    新增main的內容

---
+++ front/main.php
@@ -0,0 +1,10 @@
+<div class="hal" id="main">
+    <div>
+
+        <span style="width:18%; display:inline-block;">
+            <a href="?do=login">會員登入</a>
+        </span>
+        <div class="">
+        </div>
+    </div>
+</div>
