6a5c166dfc3784d75c88c7528acb275dc0ad8fa9
youyu <129youyu@gmail.com>
2025-02-17 16:11:11 +0800

    新增api/db的class 建立dsn pdo table __construct()  建立外部的三個function q d to

--- api/db.php
+++ api/db.php
@@ -1,5 +1,38 @@
 <?php
 date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB{
+    protected $dsn="mysql:host=localhost;charset:utf8;dbname=db0202";
+    protected $pdo;
+    protected $talbe;
+
+    function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    } 
+}
+
+// all
+// find
+// save
+// del
+
+function q($sql){
+    $dsn="mysql:host=localhost;charset:utf8;dbname=db0202";
+    $pdo=new PDO($dsn,'root','');
+}
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function to($url){
+    header("location:".$url);
+}
+
 
 
 ?>
