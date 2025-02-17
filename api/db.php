e1005ccc239223fc43dd9c4e41639c62dc38e112
youyu <129youyu@gmail.com>
2025-02-17 16:36:01 +0800

    新增db的all()

--- api/db.php
+++ api/db.php
@@ -11,12 +11,32 @@ class DB{
         $this->table=$table;
         $this->pdo=new PDO($this->dsn,'root','');
     } 
+// all
+function all(...$arg){
+    $sql="SELECT * FROM $this->table ";
+    if(!empty($arg[0])){
+        if(is_array($arg[0])){
+            $where=$this->a2s($arg[0]);
+            $sql=$sql . " WHERE " . join(" && ",$where);
+        }else{
+            $sql.=$arg[0];
+        }
+        if(!empty($arg[1])){
+            $sql= $sql . $arg[1];
+        }
+        return $this->fetchAll($sql);
+    }
+
+}
+// find
+
+// save
+
+// del
+
 }
 
-// all
-// find
-// save
-// del
+
 
 function q($sql){
     $dsn="mysql:host=localhost;charset:utf8;dbname=db0202";
