26895d019d6fff8b1239b4c42cab3913dbd3ca20
youyu <129youyu@gmail.com>
2025-02-20 16:02:44 +0800

    完成會員登入 會員註冊 忘記密碼的畫面

---
+++ front/forget.php
@@ -0,0 +1,25 @@
+<fieldset style="width: 80%;margin:10px auto;">
+    <legend>
+        忘記密碼
+    </legend>
+    <table>
+        <tr>
+            <td>
+                <p>請輸入信箱以查詢密碼</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="email" id="email">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button>尋找</button>
+            </td>
+        </tr>
+    </table>
+
+
+
+</fieldset>
---
+++ front/login.php
@@ -0,0 +1,32 @@
+<fieldset style="width: 80%;margin:10px auto;">
+    <legend>會員登入</legend>
+    <table>
+        <tr>
+            <td>
+                帳號
+            </td>
+            <td>
+                <input type="text" name="acc" id="acc">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                密碼
+            </td>
+            <td>
+                <input type="password" name="pw" id="pw">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="登入">
+                <input type="button" value="清除">
+            </td>
+            <td style="text-align: right;">
+                <a href="?do=forget">忘記密碼</a>|
+                <a href="?do=reg">尚未註冊</a>
+            </td>
+        </tr>
+    </table>
+
+</fieldset>
