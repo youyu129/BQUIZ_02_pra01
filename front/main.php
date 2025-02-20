5bb5f14e0acd49fd96dc34e11476c6e7a7e21bab
youyu <129youyu@gmail.com>
2025-02-20 14:56:55 +0800

    新增首頁的頁籤

--- front/main.php
+++ front/main.php
@@ -1,1 +1,34 @@
-頁籤
+<style>
+    . {
+        box-sizing: border-box;
+    }
+
+    .typeContainer {
+        display: flex;
+        justify-content: left;
+        margin-left: 1px;
+    }
+
+    .type {
+        padding: 5px 10px;
+        border: 1px solid #999;
+        margin-left: -1px;
+        background-color: #eee;
+    }
+
+    .container {
+        width: 90%;
+        height: 90%;
+        border: 1px solid #999;
+        margin-top: -1px;
+    }
+</style>
+<div class="typeContainer">
+    <div class="type">健康新知</div>
+    <div class="type">菸害防制</div>
+    <div class="type">癌症防治</div>
+    <div class="type">慢性病防治</div>
+</div>
+<div class="container">
+
+</div>
