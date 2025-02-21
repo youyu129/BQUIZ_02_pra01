5ee0a992e2e85076dfb502a8f56a8c573a507729
youyu <129youyu@gmail.com>
2025-02-21 16:11:02 +0800

    新增後台最新文章管理的資料顯示 分頁計算

--- back/news.php
+++ back/news.php
@@ -1,28 +1,32 @@
-<fieldset>
-    <legend>醉心文章管理</legend>
+<fieldset style="width:85%;margin:auto">
+    <legend>最新文章管理</legend>
 
-    <table width="90%" class="ct">
+    <table width="90%" class="ct" style="margin:auto;">
         <tr>
             <th width="20%">編號</th>
-            <th width="40%">標題</th>
-            <th width="20%">顯示</th>
-            <th width="20%">刪除</th>
+            <th width="50%">標題</th>
+            <th width="15%">顯示</th>
+            <th width="15%">刪除</th>
         </tr>
         <?php
-            $div  = 3;
-            $all  = $New->count();
-            $page = $all / $div;
-            $rows = $New->all();
-            foreach ($rows as $row):
+            $all   = $New->count();
+            $div   = 3;
+            $pages = ceil($all / $div);
+            $now   = $_GET['p'] ?? 1;
+            $start = ($now - 1) * $div;
+
+            $rows = $New->all(" limit $start, $div");
+            foreach ($rows as $idx => $row):
         ?>
         <tr>
-            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $start + $idx + 1 ?></td>
             <td><?php echo $row['title']; ?></td>
             <td>
-                <input type="checkbox" name="sh[]" value="">
+                <input type="checkbox" name="sh[]" value=""                                                            <?php echo($row['sh'] == 1) ? 'checked' : ''; ?>>
+
             </td>
             <td>
-                <input type="checkbox" name="del[]" value="">
+                <input type="checkbox" name="del[]" value="<?php echo $row['id']; ?>">
             </td>
         </tr>
         <?php
@@ -30,6 +34,22 @@
         ?>
     </table>
     <div class="ct">
-        <button>確定修改</button>
+
+        <?php
+            if (($now - 1) > 0) {
+                echo "<a href='?do=news&p=" . ($now - 1) . "'> < </a>";
+            }
+            for ($i = 1; $i <= $pages; $i++) {
+                $size = ($i == $now) ? '24px' : '16px';
+                echo "<a href='?do=news&p=$i' style='font-size:$size;'> $i </a>";
+            }
+            if (($now + 1) <= $pages) {
+                echo "<a href='?do=news&p=" . ($now + 1) . "'> > </a>";
+            }
+
+        ?>
+    </div>
+    <div class="ct">
+        <button onclick="edit()">確定修改</button>
     </div>
 </fieldset>
