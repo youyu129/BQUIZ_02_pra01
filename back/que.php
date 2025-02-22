4252f353083449063e8bbe4134275fa1e17ddc9a
youyu <129youyu@gmail.com>
2025-02-22 23:14:05 +0800

    新增後台問卷調查的畫面 更多按鈕的功能

--- back/que.php
+++ back/que.php
@@ -1,1 +1,34 @@
-que
+<fieldset style="width:85%;margin:auto">
+    <legend>問卷管理</legend>
+    <!-- <form action="add_que.php" method="post"> -->
+    <table>
+        <tr>
+            <td class="clo">問卷名稱</td>
+            <td>
+                <input type="text" name="" id="">
+            </td>
+        </tr>
+        <tr>
+            <td class="clo options">
+                選項<input type="text" name="options" id="">
+                <button onclick="more()">更多</button>
+            </td>
+            <td>
+
+            </td>
+        </tr>
+    </table>
+    <div>
+        <input type="submit" value="新增">|
+        <input type="reset" value="清空">
+    </div>
+    <!-- </form> -->
+</fieldset>
+
+<script>
+function more() {
+    let more = `<div>選項<input type="text" name="options" id=""><div>`
+    $(".options").prepend(more)
+    // location.reload()
+}
+</script>
