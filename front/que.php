7e1860587ca70e8645cf891c6ea14de3fd6de0d7
youyu <129youyu@gmail.com>
2025-02-24 15:40:40 +0800

    用key產生題目的編號

--- front/que.php
+++ front/que.php
@@ -3,18 +3,18 @@
 
     <table>
         <tr class="ct">
-            <td width="10%">編號</td>
-            <td width="50%">問卷題目</td>
-            <td width="10%">投票總數</td>
-            <td width="10%">結果</td>
-            <td width="10%">狀態</td>
+            <th width="10%">編號</th>
+            <th width="50%">問卷題目</th>
+            <th width="10%">投票總數</th>
+            <th width="10%">結果</th>
+            <th width="10%">狀態</th>
         </tr>
         <?php
             $rows = $Que->all(['main_id' => 0]);
-            foreach ($rows as $row):
+            foreach ($rows as $key => $row):
         ?>
         <tr style="text-align:center">
-            <td>1</td>
+            <td><?php echo $key + 1; ?>.</td>
             <td style="padding:10px;"><?php echo $row['text']; ?></td>
             <td style="padding:10px;"><?php echo $row['vote']; ?></td>
             <td>
--- front/vote.php
+++ front/vote.php
@@ -1,1 +1,19 @@
-vote
+<fieldset>
+    <legend>目前位置：首頁 > 問卷調查 > <span>Q1</span></legend>
+
+    <table>
+        <?php
+            $rows = $Que->all(['main_id' => $_POST['id']]);
+        ?>
+        <tr>
+            <td>
+                題目
+            </td>
+        </tr>
+        <tr>
+            <td>
+                選項
+            </td>
+        </tr>
+    </table>
+</fieldset>
