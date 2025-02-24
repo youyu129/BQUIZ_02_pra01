ba4a68dd0b9b648d926b7eaaea1f2a122646adea
youyu <129youyu@gmail.com>
2025-02-24 14:26:36 +0800

    在人氣文章的按讚api新增 同時將likes欄位寫入文章的資料表

--- api/like.php
+++ api/like.php
@@ -1,10 +1,14 @@
 <?php
 
 include_once "db.php";
-
-$chk=$Good->count(['news'=>$_POST['id'],'user'=>$_SESSION['login']]);
-if($chk>0){
-    $Good->del(['news'=>$_POST['id'],'user'=>$_SESSION['login']]);
-}else{
-    $Good->save(['news'=>$_POST['id'],'user'=>$_SESSION['login']]);
+$news = $New->find($_POST['id']);
+$chk  = $Good->count(['news' => $_POST['id'], 'user' => $_SESSION['login']]);
+if ($chk > 0) {
+    $Good->del(['news' => $_POST['id'], 'user' => $_SESSION['login']]);
+    $news['likes']--;
+} else {
+    $Good->save(['news' => $_POST['id'], 'user' => $_SESSION['login']]);
+    $news['likes']++;
 }
+
+$New->save($news);
