f0424d4d4428ed1c2e7757435bf7d45f66e075fb
youyu <129youyu@gmail.com>
2025-02-24 16:25:03 +0800

    建立問卷結果頁面(尚未帶入資料庫資料)

--- front/result.php
+++ front/result.php
@@ -1,1 +1,44 @@
-result
+<style>
+.box {
+    background-color: #ccc;
+}
+</style>
+<?php
+    $id   = $_GET['id'];
+    $rows = $Que->all(['id' => $id]);
+    foreach ($rows as $row) {
+        // echo $row['text'];
+    }
+?>
+<fieldset>
+    <legend>目前位置：首頁 > 問卷調查 > <span><?php echo $row['text']; ?></span></legend>
+
+
+    <div style="margin-top:20px;">
+        <?php echo $row['text']; ?>
+    </div>
+
+    <table>
+        <?php
+            $main_id = $row['id'];
+            $items   = $Que->all(['main_id' => $main_id]);
+            // dd($items);
+            foreach ($items as $item):
+        ?>
+        <tr>
+            <td width="40%"><?php echo $item['text']; ?></td>
+            <td width="40%">
+                <div class="box" style="width:40%;height:20px"></div>
+            </td>
+            <td width="20%">2票(40%)</td>
+        </tr>
+        <?php
+            endforeach;
+        ?>
+    </table>
+
+    <div style="margin-top:10px;">
+
+    </div>
+
+</fieldset>
