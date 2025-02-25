dae6cae30e6af92dc828f256cdbeb85616c40268
youyu <129youyu@gmail.com>
2025-02-25 15:13:08 +0800

    完成投票api 除了選項有記錄之外 同時在問題也記錄投票

--- api/vote.php
+++ api/vote.php
@@ -4,5 +4,7 @@ $id     = $_POST['item'];
 $option = $Que->find($id);
 $option['vote']++;
 $Que->save($option);
-
+$subject = $Que->find($option['main_id']);
+$subject['vote']++;
+$Que->save($subject);
 to("../index.php?do=result&id={$option['main_id']}");
